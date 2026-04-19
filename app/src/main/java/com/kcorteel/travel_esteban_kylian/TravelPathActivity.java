0dbf509a62c35c94d9a05b0490eacf7bdc1a99a3
TearikiZz <kcorteelpro@gmail.com>
2026-04-19 15:24:25 +0200

    initialisation partie commune

---
+++ app/src/main/java/com/kcorteel/travel_esteban_kylian/TravelPathActivity.java
@@ -0,0 +1,13 @@
+package com.kcorteel.travel_esteban_kylian;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TravelPathActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_path);
+    }
+}
---
+++ app/src/main/java/com/kcorteel/travel_esteban_kylian/TravelShareActivity.java
@@ -0,0 +1,13 @@
+package com.kcorteel.travel_esteban_kylian;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TravelShareActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_share);
+    }
+}
